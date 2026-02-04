6ceee2a51a5b36a443f5ae5e903adc73ea6cc2f5
tralalawabi-art <tralalawabi@gmail.com>
2026-02-04 20:02:36 +0700

    Delete routes directory

--- routes/tools.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const proxyController = require('../controllers/proxyController');
-
-router.get('/proxy', proxyController.handleProxy);
-
-module.exports = router;
